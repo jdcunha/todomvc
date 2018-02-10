36c112c81c790b9e76b1790c515015609f9a47de
jdcunha <james.dcunha@gmail.com>
2018-02-10 00:43:48 -0500

    Update app.js
    Created a App.saveTodosToLocalStorage() which saves the todo list to local storage
    Having App.saveTodosToLocalStorage() called prior to the rendor function

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -70,7 +70,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +89,7 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+			this.saveTodosToLocalStorage();
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +116,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			this.saveTodosToLocalStorage();
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -147,12 +147,13 @@ jQuery(function ($) {
 			});
 
 			$input.val('');
-
+			this.saveTodosToLocalStorage();
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			this.saveTodosToLocalStorage();
 			this.render();
 		},
 		edit: function (e) {
@@ -183,12 +184,16 @@ jQuery(function ($) {
 			} else {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
-
+			this.saveTodosToLocalStorage();
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.saveTodosToLocalStorage();
 			this.render();
+		},
+		saveTodosToLocalStorage: function(){
+		    util.store('todos-jquery', this.todos);
 		}
 	};
 
